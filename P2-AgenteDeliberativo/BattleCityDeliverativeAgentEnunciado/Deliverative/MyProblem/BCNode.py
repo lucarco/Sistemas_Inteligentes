3d2408dd30a95ec34ae18e06bf5103347aa7c853
el-lucass <arranzluc@gmail.com>
2026-04-01 14:08:07 +0200

    renombrar y init P2

---
+++ P2-AgenteDeliberativo/BattleCityDeliverativeAgentEnunciado/Deliverative/MyProblem/BCNode.py
@@ -0,0 +1,26 @@
+from AStar.Node import Node
+
+class BCNode(Node):
+    def __init__(self, parent, g, value, x, y):
+        super().__init__(parent, g)
+        self.value = value
+        self.x = int(x)
+        self.y = int(y)
+    
+    def __repr__(self):
+        return f"BCNode(x={self.x}, y={self.y})"
+
+    def __eq__(self, other):
+        if other == None:
+            return False
+        #TODO: dos nodos son iguales cuando sus coordenadas x e y son iguales.
+        print("Aqui falta ncosas por hacer :) ")
+        return False
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+    
+
+
+
+    
